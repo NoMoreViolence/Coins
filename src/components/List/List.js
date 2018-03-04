a89a58ed995490367ddb1b6e5be9b310b2184d0c
NoMoreViolence <ljh86029926@gmail.com>
2018-03-04 16:43:22 +0900

    Merge Search components & List components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -9,39 +9,66 @@ class List extends Component {
     // initializes component state
     this.state = {
       fetching: false, // 요청이 끝났으면 false
-      results: []
+      results: [], // 코인들의 정보가 들어가게 되는 곳
+      searchkey: '' // 검색시 글자 들어가는 곳
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  // 전체 코인의 받는 메소드
   Coins = async coin => {
     this.setState({
       fetching: true // requesting..
     });
 
     const post = await Call.GetAll();
-    console.log(post.data.result);
 
     this.setState({
       results: post.data.result,
       fetching: false
     });
+    console.log(this.state.results);
   };
 
+  // 처음 로딩되었을 때 코인 정보 출력 실행
   componentDidMount() {
     this.Coins();
   }
 
+  handleChange(e) {
+    this.setState({
+      searchkey: e.target.value
+    });
+  }
+
   render() {
+    const lists = data => {
+      data.sort();
+
+      data = data.filter(info => {
+        return (
+          info.symbol
+            .toLowerCase()
+            .indexOf(this.state.searchkey.toLowerCase()) > -1
+        );
+      });
+
+      return data.map((info, i) => {
+        return <ListForm symbol={info.symbol} name={info.name} key={i} />;
+      });
+    };
+
     return (
       <div>
         <div className="SearchContainer">
-          <input type="text" placeholder="Input Your Coin Initial" />
+          <input
+            type="text"
+            placeholder="Input Your Coin Initial"
+            value={this.state.searchkey}
+            onChange={this.handleChange}
+          />
         </div>
-        <div className="List">
-          {this.state.results.map((info, i) => {
-            return <ListForm symbol={info.symbol} name={info.name} key={i} />;
-          })}
-        </div>
+        <div className="List">{lists(this.state.results)}</div>
       </div>
     );
   }
