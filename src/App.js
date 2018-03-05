3b09016c93f2706fd4e78ad0a92cc77d056a0cd3
NoMoreViolence <ljh86029926@gmail.com>
2018-03-05 09:36:05 +0900

    Add Select component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import styled from 'styled-components';
+// 검색 시
+import Select from './components/Select/Select';
 // 검색 바
 import List from './components/List/List';
 
@@ -16,6 +18,7 @@ class App extends Component {
       <BrowserRouter>
         <Container>
           <Switch>
+            <Route path="/:CoinName" component={Select} />
             <Route path="/" component={List} />
           </Switch>
         </Container>
