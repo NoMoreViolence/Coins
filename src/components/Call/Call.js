ab64a4115a5904440f1acd51e1c6e5c537c949cd
NoMoreViolence <ljh86029926@gmail.com>
2018-03-07 23:31:38 +0900

    API Each coins infomation of price & market

--- src/components/Call/Call.js
+++ src/components/Call/Call.js
@@ -1,9 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
+// 모든 코인의 이름 받을 때
 export function GetAll() {
-  return axios.get("https://api.cryptowat.ch/assets");
+  return axios.get('https://api.cryptowat.ch/assets');
 }
 
+// 코인별 마켓 정보 받을 때
 export function GetCoin(coin) {
   return axios.get(`https://api.cryptowat.ch/assets/${coin}`);
+}
+
+// 코인 별 가격 정보
+export function GetPriceOfCoin(market, exchange) {
+  return axios.get(
+    `https://api.cryptowat.ch/markets/${market}/${exchange}/summary`
+  );
 }
