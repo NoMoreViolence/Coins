7353a4381d66f609dfba3d26e2c91b1a7b91accf
NoMoreViolence <ljh86029926@gmail.com>
2018-03-12 20:33:42 +0900

    API heroku API server

--- src/components/Call/Call.js
+++ src/components/Call/Call.js
@@ -2,17 +2,15 @@ import axios from 'axios';
 
 // 모든 코인의 이름 받을 때
 export function GetAll() {
-  return axios.get('https://api.cryptowat.ch/assets');
+  return axios.get('http://localhost:5000/api');
 }
 
 // 코인별 마켓 정보 받을 때
 export function GetCoin(coin) {
-  return axios.get(`https://api.cryptowat.ch/assets/${coin}`);
+  return axios.get(`http://localhost:5000/api/${coin}`);
 }
 
 // 코인 별 가격 정보
-export function GetPriceOfCoin(market, exchange) {
-  return axios.get(
-    `https://api.cryptowat.ch/markets/${market}/${exchange}/summary`
-  );
+export function GetPriceOfCoin(coin, exchange) {
+  return axios.get(`http://localhost:5000/api/${coin}/${exchange}`);
 }
